489a8f00c57b4b4974b5f4ed3fed7ad2b34e330b
barath923 <124130166+barath923@users.noreply.github.com>
2023-11-30 13:07:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,32 +15,8 @@ provider "azurerm" {
   }
 }
 
-# Vnet resource creation
-resource "azurerm_virtual_network" "HT_Vnet" {
-  name                = "ht-terraform-vnet"
-  location            = "Central India"
-  resource_group_name = "rg1"
-  address_space       = ["10.10.0.0/16"]
-
-  subnet {
-    name           = "subnet1"
-    address_prefix = "10.10.1.0/24"
-  }
-
-  subnet {
-    name           = "subnet2"
-    address_prefix = "10.10.2.0/24"
-  }
-
-  subnet {
-    name           = "subnet3"
-    address_prefix = "10.10.3.0/24"
-  }
-}
-
-
-resource "azurerm_network_security_group" "HT_SG" {
-  name                = "Terraform-security-group"
-  location            = "Central India"
-  resource_group_name = "rg1"
+# resource Group creation
+resource "azurerm_resource_group" "example" {
+  name     = "example"
+  location = "West Europe"
 }
